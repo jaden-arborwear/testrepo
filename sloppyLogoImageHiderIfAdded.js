fdcd5b146b8065d4158ae37bedadd6f2076d5b49
Jaden DeVogel <jdevogel@JadensMac.local>
2018-12-12 11:54:12 -0500

    test commit of logo image hider with better updates

--- sloppyLogoImageHiderIfAdded.js
+++ sloppyLogoImageHiderIfAdded.js
@@ -11,4 +11,25 @@ window.setInterval(function(){
   hideImage();
 }, 250);
 
-// this thing is nasty
+/* this thing above is nasty, would smomething like this work instead?
+
+(function(doc,found) {
+  window.addEventListener('DOMSubtreeModified', function() {
+
+    var img = document.getElementById('product_corporate_logo');
+    var path = '/pub/media/itoris/files/w/h/white-nelson-logo-on-red-apparel_rs.png';
+
+    if(found && !img && !img.getAttribute('src').indexOf(path) > -1){
+      found = false;
+    }
+
+    if(img && img.getAttribute('src').indexOf(path) > -1){
+      found = true;
+      img.style.visibility = 'hidden';
+      img.parentNode.removeChild(img);
+    }
+
+  }, false);
+})(document,false);
+
+*/
